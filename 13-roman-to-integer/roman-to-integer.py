58cbece13a8a46d83ee8805be9065116a25d047c
likhith0501 <hplikhith033@gmail.com>
2026-09-25 07:49:45 +0530

    Time: 9 ms (19.21%) | Memory: 19.3 MB (59.97%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            # If the current symbol is worth less than the next symbol,
+            # it indicates a subtractive case (e.g., IV = 4, IX = 9)
+            if i + 1 < n and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+                
+        return total
